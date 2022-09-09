78f45d565889d3109f7ef0b299d77ca44d5b70a0
shaqxPL <tomasz198720@gmail.com>
2022-09-09 17:30:51 +0200

    switch different cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//	int num = 1;
+//    switch (num){
+//        case 1:
+//            System.out.println("case 1: value is:" + num);
+//
+//        case 2:
+//            System.out.println("case 2: value is:" + num);
+//
+//        case 3:
+//            System.out.println("case 3: value is:" + num);
+//
+//        default:
+//            System.out.println("default: value is:" + num);
+//    }
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+//        System.out.println("Enter the 4 numbers password");
+//        int password = 1234;
+//        switch (password) {
+//            case 1234:
+//                System.out.println("good password");
+//                break;
+//            default:
+//                System.out.println("wrong password");
+//        }
+
     }
 }
